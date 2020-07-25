947442284d262a8f15a548f946636dd5646aad53
Michael Dresser <michaelmdresser@gmail.com>
2020-07-25 00:00:10 -0600

    Full basic site

---
+++ teemo.ts
@@ -0,0 +1,48 @@
+interface TeamBetsResponse {
+    bets: number[]
+}
+
+async function getTeamBets(team: string): Promise<TeamBetsResponse> {
+    return fetch("http://192.168.1.111:3002/bets/" + team, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => {
+        return res.json().then(d => d as TeamBetsResponse)
+    })
+}
+
+function getTeamBetsTotal(bets: TeamBetsResponse): number {
+    return bets.bets.reduce((a, b) => a + b)
+}
+
+function makeIndividualBetsDiv(bets: number[]) {
+    let div = document.createElement("div")
+
+    bets.sort(function (a, b) {return b - a})
+    bets.map(amount => {
+        let child = document.createElement("div")
+        child.innerText = String(amount)
+        div.appendChild(child)
+    })
+
+    return div
+}
+
+function updateBetInfo(team: string) {
+    const divName = team + "-bet-total"
+    let totalDiv = document.getElementById(divName)
+    let individualDiv = document.getElementById(team + "-individual-bets")
+
+    getTeamBets(team).then(response => {
+        totalDiv.innerText = String(getTeamBetsTotal(response))
+        individualDiv.innerHTML = ''
+        individualDiv.appendChild(makeIndividualBetsDiv(response.bets))
+    })
+}
+
+updateBetInfo("red")
+updateBetInfo("blue")
+setInterval(function(){ updateBetInfo("red"); updateBetInfo("blue") }, 3000)
